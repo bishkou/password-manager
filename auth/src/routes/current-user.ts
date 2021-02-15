85ffe60c6b5bdef24473bbabcfb24cf749018431
bishkou <chedyhammami@yahoo.com>
2021-02-15 23:07:53 +0100

    current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -8,5 +8,4 @@ router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
-
 export { router as currentUserRouter };
